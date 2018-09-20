14b2409b74b16c4ef4f61e8c4bfe70051f5042ab
Nicolai Davies <Nicolai.d@hotmail.com>
2018-09-20 12:00:24 +0200

    Sketch endpoints

--- src/v8/controllers/user/find.js
+++ src/v8/controllers/user/find.js
@@ -1,3 +1,24 @@
-module.exports = async function find(req, res) {
+const ObjectID = require('mongodb').ObjectID
 
+/**
+ * @api {post} /user Create user
+ * @apiName Create User
+ * @apiGroup User
+ *
+ * @apiParam {string} instance_id The id for the instance
+ */
+
+module.exports = async function find(req, res) {
+  const instance_id = req.query['instance_id']
+
+  if (!instance_id) {
+    return res.status(400).send({error: "instance_id is required"})
+  }
+
+  // TODO: Check if user is admin or super-admin
+  
+  // Might have to first find permissions, if we go with new approach suggested
+  const users = await req.db.collection('users').find({})
+
+  res.send(users)
 }
--- src/v8/controllers/user/remove.js
+++ src/v8/controllers/user/remove.js
@@ -1,3 +1,22 @@
-module.exports = async function remove(req, res) {
+const ObjectID = require('mongodb').ObjectID
+/**
+ * @api {delete} /user/:id Remove user
+ * @apiName Remove User
+ * @apiGroup User
+ *
+ * @apiParam {string} id The of the user to remove
+ */
 
+module.exports = async function remove(req, res) {
+  const user_id_to_remove = req.params['id']
+
+  if (!user_id_to_remove) {
+    return res.status(400).send({error: "user id is required"})
+  }
+
+  // TODO: Check if user is admin or super-admin
+
+  await req.db.collection('users').deleteOne({ _id: ObjectID(user_id_to_remove)})
+
+  res.send({status: 'successfully removed user'})
 }
