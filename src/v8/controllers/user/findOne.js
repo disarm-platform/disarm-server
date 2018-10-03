4c1847cf9b56e31ad89bb85066b0d0ff082ce005
Nicolai Davies <Nicolai.d@hotmail.com>
2018-10-03 09:23:13 +0200

    Return 401 if not allowed to get user

--- src/v8/controllers/user/findOne.js
+++ src/v8/controllers/user/findOne.js
@@ -1,4 +1,5 @@
 const ObjectID = require('mongodb').ObjectID
+const { can } = require('../../lib/helpers/can')
 
 module.exports = async function findOne(req, res) {
   const user_id = req.params.user_id
@@ -7,9 +8,21 @@ module.exports = async function findOne(req, res) {
     return res.status(400).send()
   }
 
-  // const incoming_user_permissions = await req.db.collection('permissions').find({}).toArray()
 
-  // const logged_in_user_permissions = await req.db.collection('permissions').find({}).toArray()
+  const user_permissions = await req.db.collection('permissions').find({ user_id: found_user._id }).toArray()
+  const user_instances = user_permissions.map(p => p.instance_id)
+
+  let allowed_to_edit_user = false
+  for (const instance_id of user_instances) {
+    const allowed = await can(req.user._id, instance_id)
+    if (allowed) {
+      allowed_to_edit_user = true
+    }
+  }
+
+  if (!allowed_to_edit_user) {
+    return res.status(401).send()
+  }
 
   // TODO: Do some sort of overlap checking, should probably find different approach
 
