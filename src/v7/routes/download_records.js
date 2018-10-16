b57b10bd106a0273b95663e04a0f136aaa288d66
mosaic141688 <mbhamalimosaic@gmail.com>
2018-10-16 16:42:21 +0200

    Checking download_key

--- src/v7/routes/download_records.js
+++ src/v7/routes/download_records.js
@@ -1,8 +1,20 @@
+const stream = require('stream')
+const {findByApiKey} = require('../lib/auth')
 const  {process} = require('../lib/csv-export/process')
 module.exports = async (req, res) => {
     const records_collection = req.db.collection('records')
     const country = req.country
     const personalised_instance_id = req.personalised_instance_id
+    const download_key = req.query.download_key;
+
+
+    if(!download_key){
+        return res.status(403).send('Forbidden')
+    }
+    if(!findByApiKey(download_key)){
+        return res.status(403).send('Forbidden')
+    }
+
     try {
         const records = await records_collection
             .find({country, personalised_instance_id})
@@ -10,10 +22,14 @@ module.exports = async (req, res) => {
             .toArray()
         console.log('records',records)
         const csv_records = process(records)
-        res.send(csv_records);
+        var readStream = new stream.PassThrough();
+        readStream.end(csv_records);
+        res.set('Content-Type','Application/csv')
+        res.set('Content-disposition', 'attachment; filename=records.csv');
+        readStream.pipe(res);
 
     } catch (e) {
-        res.status(500).send(e.message);
+        res.status(400).send('Error');
     }
 
 }
