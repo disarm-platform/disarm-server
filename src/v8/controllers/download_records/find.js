16e581a026c78edfd80bd9b3f9ea7782e6dcd630
mosaic141688 <mbhamalimosaic@gmail.com>
2018-10-17 14:02:11 +0200

    Using download key

--- src/v8/controllers/download_records/find.js
+++ src/v8/controllers/download_records/find.js
@@ -1,31 +1,39 @@
 const  {process} = require('../../lib/helpers/csv-export/process')
-const {findByApiKey} = require('../lib/auth')
+const {findByApiKey} = require('../../lib/auth')
 const ObjectID = require('mongodb').ObjectID
+const stream = require('stream')
+
 const {can_any} = require('../../lib/helpers/can')
 
 module.exports = async (req, res) => {
     const records_collection = req.db.collection('records')
     const personalised_instance_id = req.query.personalised_instance_id
-    const download_key = req.query.download_key;
+    const download_key = req.query.download_key || req.get('API-Key');
+    console.log(download_key)
 
     const instance_id = ObjectID(req.query.instance_id)
     const instance = await req.db.collection('instances').findOne({_id: ObjectID(instance_id)})
 
-
     if(!download_key){
+        console.log('No download key',download_key)
         return res.status(403).send('Forbidden')
     }
-    if(!findByApiKey(download_key)){
+
+    const users_collection = req.db.collection('sessions')
+
+    const user = await users_collection.findOne({api_key:download_key})
+
+    if(!user){
+        console.log('Cant find by key')
         return res.status(403).send('Forbidden')
     }
+
 
     if (!instance) {
         return res.status(400).send()
     }
 
-    if (!allowed) {
-        return res.status(401).send()
-    }
+
     try {
         const records = await records_collection
             .find({instance_id, personalised_instance_id})
@@ -39,6 +47,7 @@ module.exports = async (req, res) => {
         readStream.pipe(res);
 
     } catch (e) {
+        console.log(e)
         res.status(500).send(e.message);
     }
 
