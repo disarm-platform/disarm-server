5a65cf07f517b226ab75837c41d2f9ef3c5224c6
mosaic141688 <mbhamalimosaic@gmail.com>
2018-10-17 14:03:39 +0200

    Corrected tests to expect status 403

--- test/v8/download_records/find.test.js
+++ test/v8/download_records/find.test.js
@@ -14,7 +14,7 @@ test('get from /download_records returns 401 when not logged in', async t => {
     const res = await request(app).get('/v8/download_records')
         .send({})
 
-    t.is(res.status, 401)
+    t.is(res.status, 403)
 })
 
 test('get from /download_records with loged in user returns 401 if no instance_id', async t => {
@@ -22,9 +22,9 @@ test('get from /download_records with loged in user returns 401 if no instance_i
     const user = await create_user()
 
     const res = await request(app).get(`/v8/download_records`)
-        .set('API-key', user.key)
+        //.set('API-key', user.key)
 
-    t.is(res.status, 400)
+    t.is(res.status, 403)
 })
 
 test('GET from /download_records with admin user  returns csv of records', async t => {
@@ -57,8 +57,7 @@ test('GET from /download_records with admin user  returns csv of records', async
         value: 'read:irs_records_point'
     })
 
-    const res = await request(app).get(`/v8/download_records?instance_id=${instance_id}&personalised_instance_id=${personalised_instance_id}`)
-        .set('API-key', user.key)
+    const res = await request(app).get(`/v8/download_records?download_key=${user.key}&instance_id=${instance_id}&personalised_instance_id=${personalised_instance_id}`)
 
     t.is(res.status, 200)
 
@@ -95,9 +94,46 @@ test('get from /download_records with user with read:irs_record_point returns a 
         value: 'read:irs_records_point'
     })
 
-    const res = await request(app).get(`/v8/download_records?instance_id=${instance_id}&personalised_instance_id=${personalised_instance_id}`)
-        .set('API-key', user.key)
+    const res = await request(app).get(`/v8/download_records?download_key=${user.key}&instance_id=${instance_id}&personalised_instance_id=${personalised_instance_id}`)
 
+    t.is(res.status, 200)
+
+    t.true(res.text.startsWith(`"_id","personalised_instance_id","instance_id","updated_at","form_field"`))
+})
+
+
+test('get from /download_records with user with read:irs_record_point with api_keyreturns a csv table of records', async t => {
+    const db = await get_db()
+    const user = await create_user()
+
+    const personalised_instance_id = 'default'
+
+    const {insertedId: instance_id} = await db.collection('instances').insertOne({
+        name: 'my_instance_thing'
+    })
+
+    await db.collection('records').insertOne({
+        personalised_instance_id,
+        instance_id,
+        updated_at: +(new Date),
+        form_field: 'field 1'
+    })
+
+    await db.collection('records').insertOne({
+        personalised_instance_id,
+        instance_id,
+        updated_at: +(new Date),
+        form_field: 'field 2'
+    })
+
+    await db.collection('permissions').insertOne({
+        user_id: user._id,
+        instance_id,
+        value: 'read:irs_records_point'
+    })
+
+    const res = await request(app).get(`/v8/download_records?instance_id=${instance_id}&personalised_instance_id=${personalised_instance_id}`)
+        .set('API-Key',user.key)
     t.is(res.status, 200)
 
     t.true(res.text.startsWith(`"_id","personalised_instance_id","instance_id","updated_at","form_field"`))
