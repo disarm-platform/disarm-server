cffc76776f033e2368fcb510c01b8f9267781392
mosaic141688 <mbhamalimosaic@gmail.com>
2018-11-30 16:44:46 +0200

    Using find

--- src/v8/controllers/config/find.js
+++ src/v8/controllers/config/find.js
@@ -12,7 +12,7 @@ const { is_user } = require('../../lib/helpers/is_user')
 module.exports = async function findOne(req, res) {
     const instance_id = req.query.instance_id
     try{
-        const configs = await req.db.collection('instance_configs').findOne({ instance_id: ObjectID(instance_id)}).toArray()
+        const configs = await req.db.collection('instance_configs').find({ instance_id: ObjectID(instance_id)},{}).toArray()
 
         const allowed = await is_user(req.user._id, instance_id)
         if (!allowed) {
