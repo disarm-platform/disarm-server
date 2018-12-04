7fc1b1111afc19ed8370a10eca248a1cf96c36f0
mosaic141688 <mbhamalimosaic@gmail.com>
2018-12-04 12:17:06 +0200

    Implemented findLatest config controller

--- src/v8/controllers/config/findLatest.js
+++ src/v8/controllers/config/findLatest.js
@@ -1,3 +1,15 @@
-module.exports = async function findLatest(req,res){
-// TODO Implememnt
+const ObjectID = require('mongodb').ObjectID
+const { is_user } = require('../../lib/helpers/is_user')
+
+module.exports = async function findLatest(req,res) {
+    const {instance_id} = req.query;
+
+    const allowed = await is_user(req.user._id, instance_id)
+    if (!allowed) {
+        return res.status(401).send({ error: 'Not authorized' })
+    }
+    
+    const latest_config = await req.db.collection('instance_configs')
+        .find({instance_id: ObjectID(instance_id)}).sort({_id: -1}).limit(1).toArray()
+    res.send(latest_config.pop())
 }
