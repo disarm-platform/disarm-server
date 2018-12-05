a903615224d256577c3b3f486826764ab44348d6
mosaic141688 <mbhamalimosaic@gmail.com>
2018-12-05 11:12:24 +0200

    Updated findGeodataSummary

--- src/v8/controllers/geodata/findSummary.js
+++ src/v8/controllers/geodata/findSummary.js
@@ -1,5 +1,5 @@
 const ObjectID = require('mongodb').ObjectID
-const { is_user } = require('../../lib/helpers/is_user')
+const {is_user} = require('../../lib/helpers/is_user')
 
 /**
  * @api {get} /geodata/:level_name Create geodata level
@@ -10,27 +10,28 @@ const { is_user } = require('../../lib/helpers/is_user')
  */
 
 
-module.exports = async function findOne(req, res) {
-    const level_name = req.params['level_name']
+module.exports = async function findSummary(req, res) {
     const instance_id = req.params['instance_id']
 
     const allowed = await is_user(req.user._id, instance_id)
     if (!allowed) {
-        return res.status(401).send({ error: 'Not authorized' })
+        return res.status(401).send({error: 'Not authorized'})
     }
 
-    const geodata_level_sumaries = await req.db.collection('geodata')
-        .find({
-            instance_id: ObjectID(instance_id),
-            level_name
-        }, {
-            geojson: 0 // Do not include geojson field
-        }).toArray()
+    const query = {instance_id: ObjectID(instance_id)}
 
+    const unique_levels = await req.db.collection('geodata').distinct('level_name',query).toArray()
 
-    if(!geodata_level_sumaries[0]){
-        return res.send([])
-    }
+    const geodata_level_summaries = unique_levels.reduce(async (acc,level_name) => {
+        const level_query = {...query,level_name}
+        const level_summaries = await req.db.collection('geodata')
+            .find(level_query,
+                {
+                    geojson: 0 // Do not include geojson field
+                }
+            ).limit(5).toArray()
+        return acc.concat(level_summaries)
+    },[])
 
-    res.send(geodata_level_sumaries)
+    res.send(geodata_level_summaries)
 }
--- src/v8/controllers/geodata/upload.js
+++ src/v8/controllers/geodata/upload.js
@@ -2,32 +2,24 @@ const ObjectID = require('mongodb').ObjectID
 const {can} = require('../../lib/helpers/can')
 const {summarise, validate_layer_schema} = require("@disarm/geodata-support");
 module.exports = async function upload(req, res) {
+
     const instance_id = req.params['instance_id']
-
     const {name,data} = req.files.file
-    _data = JSON.parse(data.toString())
-    const summary = summarise(_data)
 
-    const geodata_level_with_highest_version = await req.db.collection('geodata')
-        .findOne({
+    const _data = JSON.parse(data.toString())
+    try {
+        const summary = summarise(_data)
+
+        await req.db.collection('geodata').insertOne({
             instance_id: ObjectID(instance_id),
-            level_name:name
-        }, {
-            sort: {version: -1},
-            limit: 1
+            level_name:name,
+            summary,
+            geojson:_data
         })
 
-    // if there is an existing geodata level we bump the version, if not we start at 1
-    const new_version = geodata_level_with_highest_version ? geodata_level_with_highest_version.version + 1 : 1
+        res.send(summary);
+    }catch (e) {
+        res.status(400).send('Geodata nos saved')
+    }
 
-
-    await req.db.collection('geodata').insertOne({
-        version: new_version,
-        instance_id: ObjectID(instance_id),
-        level_name:name,
-        summary,
-        geojson:_data
-    })
-
-    res.send(summary);
 }
