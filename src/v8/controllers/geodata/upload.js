56d65531c08f0208d54f8a01f155d8d7640b3c63
Jonathan Smith <jonathan@peoplesized.com>
2018-12-05 14:13:49 +0200

    change incoming properties

--- src/v8/controllers/geodata/upload.js
+++ src/v8/controllers/geodata/upload.js
@@ -1,20 +1,24 @@
 const ObjectID = require('mongodb').ObjectID
 const {can} = require('../../lib/helpers/can')
 const {summarise, validate_layer_schema} = require("@disarm/geodata-support");
+
 module.exports = async function upload(req, res) {
 
-    const instance_id = req.params['instance_id']
-    const {name,data} = req.files.file
+    const instance_id = req.query['instance_id']
+    const level_name = req.query['level_name']
+    const {filename, data} = req.files.file
 
     const _data = JSON.parse(data.toString())
+
     try {
         const summary = summarise(_data)
 
         await req.db.collection('geodata').insertOne({
             instance_id: ObjectID(instance_id),
-            level_name:name,
+            level_name: level_name,
+            filename: filename,
             summary,
-            geojson:_data
+            geojson: _data
         })
 
         res.send(summary);
