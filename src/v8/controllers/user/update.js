db2a327925222ca8f7ba664a4fc80c9da8cfe61b
mosaic141688 <mbhamalimosaic@gmail.com>
2018-12-06 16:09:18 +0200

    Encrypting password in incoming user

--- src/v8/controllers/user/update.js
+++ src/v8/controllers/user/update.js
@@ -1,25 +1,17 @@
 const ObjectID = require('mongodb').ObjectID
 const { can } = require('../../lib/helpers/can')
+const bcrypt = require('bcryptjs')
 
 module.exports = async function update(req, res) {
-  const user_id = req.params.user_id
+  const incoming_user = req.body;
   
-  const user = await req.db.collection('users').findOne({_id: ObjectID(user_id)})
+  const user = await req.db.collection('users').findOne({_id: ObjectID(incoming_user._id)})
   if (!user) {
     return res.status(400).send()
   }
 
+  let allowed_to_edit_user = req.user.deployment_admin
 
-  const user_permissions = await req.db.collection('permissions').find({user_id: user._id}).toArray()
-  const user_instances = user_permissions.map(p => p.instance_id)
-
-  let allowed_to_edit_user = false
-  for (const instance_id of user_instances) {
-    const allowed = await can(req.user._id, instance_id)
-    if (allowed) {
-      allowed_to_edit_user = true
-    }
-  }
 
   if (!allowed_to_edit_user) {
     return res.status(401).send()
@@ -30,7 +22,12 @@ module.exports = async function update(req, res) {
     return res.status(400).send()
   }
 
-  await req.db.collection('users').update({_id: user._id}, {$set: {username}})
+  incoming_user.encrypted_password =  await bcrypt.hash(incoming_user.password, 10)
+
+  delete incoming_user._id
+  delete incoming_user.password
+
+  await req.db.collection('users').updateOne({_id:user._id},incoming_user)
 
   res.send()
 }
