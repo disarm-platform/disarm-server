30c93a4f403dcea54b6fb75dbe580de2796eb87b
Jonathan Smith <jonathan@peoplesized.com>
2018-12-12 16:31:05 +0200

    lint

--- src/v8/controllers/config/findLatest.js
+++ src/v8/controllers/config/findLatest.js
@@ -1,15 +1,23 @@
 const ObjectID = require('mongodb').ObjectID
-const { is_user } = require('../../lib/helpers/is_user')
+const {
+    is_user
+} = require('../../lib/helpers/is_user')
 
-module.exports = async function findLatest(req,res) {
-    const {instance_id} = req.query;
+module.exports = async function findLatest(req, res) {
+    const { instance_id } = req.query;
 
     const allowed = await is_user(req.user._id, instance_id)
     if (!allowed) {
-        return res.status(401).send({ error: 'Not authorized' })
+        return res.status(401).send({
+            error: 'Not authorized'
+        })
     }
-    
+
     const latest_config = await req.db.collection('instance_configs')
-        .find({instance_id}).sort({_id: -1}).limit(1).toArray()
+        .find({
+            instance_id: ObjectID(instance_id)
+        }).sort({
+            _id: -1
+        }).limit(1).toArray()
     res.send(latest_config.pop())
 }
